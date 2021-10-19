b56a33e9187c7948ad98bda89fc13486ce5fe271
ItsFlo <flo@bosstrowski.de>
2021-10-19 16:45:30 +0200

    added Dispatcher for ServerSentEvents

---
+++ modules/SseDispatcher.js
@@ -0,0 +1,70 @@
+import HttpDispatcher from "./HttpDispatcher.js";
+
+class SseDispatcher extends HttpDispatcher {
+	maConnections = [];
+
+	request(sPath, request, response, ...args) {
+		if(sPath || request.headers.accept !== "text/event-stream") {
+			super.request(sPath, request, response, ...args);
+			return;
+		}
+		response.setHeader("Cache-Control", "no-cache");
+		response.setHeader("Content-Type", "text/event-stream");
+		response.writeHead(200);
+		response.write("");
+
+		response.on("close", () => {
+			let iIndex = this.maConnections.indexOf(response);
+			this.maConnections.splice(iIndex, 1);
+		});
+		this.maConnections.push(response);
+	}
+
+
+	closeAllConnections() {
+		for(let oConnection of this.maConnections) {
+			oConnection.end();
+		}
+		this.maConnections = [];
+		return this;
+	}
+
+
+	formatEvent(sEvent) {
+		let iNewLineIndex = sEvent.indexOf("\n");
+		if(iNewLineIndex !== -1) {
+			sEvent = sEvent.substring(0, iNewLineIndex);
+		}
+		return "event: " + sEvent;
+	}
+	formatData(sData) {
+		return "data: " + sData.split("\n").join("\ndata: ");
+	}
+
+	send(sData) {
+		if(!sData || typeof sData !== "string") {
+			return;
+		}
+		let sMessage = this.formatData(sData);
+
+		sMessage += "\n\n";
+		for(let oConnection of this.maConnections) {
+			oConnection.write(sMessage);
+		}
+	}
+	sendEvent(sEventName, sData) {
+		if(!sEventName || typeof sEventName !== "string" || !sData || typeof sData !== "string") {
+			return;
+		}
+		let sMessage = this.formatEvent(sEventName);
+		sMessage += "\n" + this.formatData(sData);
+
+		sMessage += "\n\n";
+		for(let oConnection of this.maConnections) {
+			oConnection.write(sMessage);
+		}
+	}
+};
+
+
+export default SseDispatcher;
