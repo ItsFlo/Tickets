932aef04c8d29024fdc1fff79a4fff8e66c9d142
ItsFlo <flo@bosstrowski.de>
2022-04-05 00:26:49 +0200

    Move order html element to seperate module

--- tickets/script/checkout/openOrders.js
+++ tickets/script/checkout/openOrders.js
@@ -1,32 +1,8 @@
 import Api from "../Api.js";
 import Error from "../Error.js";
+import OrderElement from "../modules/orderElement.js";
 
 let oEventSource = null;
-
-function formatDate(date) {
-	let dateString = date.getFullYear() + "-";
-	if(date.getMonth() < 9) {
-		dateString += "0";
-	}
-	dateString += (date.getMonth()+1) + "-";
-	if(date.getDate() < 10) {
-		dateString += "0";
-	}
-	dateString += date.getDate();
-	return dateString;
-}
-function formatTime(date) {
-	let sTimeString = date.getHours() + ":";
-	if(date.getMinutes() < 10) {
-		sTimeString += "0";
-	}
-	sTimeString += date.getMinutes() + ":";
-	if(date.getSeconds() < 10) {
-		sTimeString += "0";
-	}
-	sTimeString += date.getSeconds();
-	return sTimeString;
-}
 
 
 
@@ -53,7 +29,7 @@ function openEventSource(iVenueID) {
 function newOrderEventListener(ev) {
 	try {
 		let oOrder = JSON.parse(ev.data);
-		let oOrderElement = createElement(oOrder);
+		let oOrderElement = createOrderElement(oOrder);
 		insertElement(oOrderElement);
 	} catch(err) {
 		Error.show(err);
@@ -85,7 +61,7 @@ function orderCancelEventListener(ev) {
 	try {
 		let order = JSON.parse(ev.data);
 		let orderElement = getOrderByID(order.id);
-		updateOrderElementMetaData(orderElement, order);
+		OrderElement.updateOrderData(orderElement, order);
 	} catch(err) {
 		Error.show(err);
 	}
@@ -115,7 +91,6 @@ function pickedUpListener() {
 
 	Api.order.setPickedUp(iOrderID).catch(err => Error.show(err.message));
 }
-
 function cancelListener() {
 	let order = this.closest(".order");
 	let orderId = parseInt(order.dataset.orderId);
@@ -132,83 +107,16 @@ function cancelListener() {
 		order.remove();
 	}
 }
+function createOrderElement(order=null) {
+	let element = OrderElement.createOrderElement(order);
+	element.querySelector("input.prepared").addEventListener("click", preparedListener);
+	element.querySelector("input.pickedup").addEventListener("click", pickedUpListener);
+	element.querySelector(".cancel-button").addEventListener("click", cancelListener);
+	return element;
+}
 
 function getOrderByID(iOrderID) {
 	return document.querySelector("#openOrders > .order[data-order-id=\""+iOrderID+"\"");
-}
-
-
-function createItemElement(oItem) {
-	let oElement = document.createElement("div");
-	oElement.classList.add("item");
-
-	let oCount = document.createElement("span");
-	oCount.innerHTML = oItem.count + "x ";
-	oElement.appendChild(oCount);
-
-	let oName = document.createElement("span");
-	oName.innerHTML = oItem.name;
-	oElement.appendChild(oName);
-
-	return oElement;
-}
-
-let template = null;
-function getTemplate() {
-	if(!template) {
-		template = document.getElementById("orderTemplate").content.firstElementChild;
-	}
-	return template.cloneNode(true);
-}
-function createElement(order) {
-	let element = getTemplate();
-
-	let preparedButton = element.querySelector("input.prepared");
-	preparedButton.addEventListener("click", preparedListener);
-
-	let pickedUpButton = element.querySelector("input.pickedup");
-	pickedUpButton.addEventListener("click", pickedUpListener);
-
-	let cancelButton = element.querySelector(".cancel-button");
-	cancelButton.addEventListener("click", cancelListener);
-
-
-	updateOrderElementMetaData(element, order);
-	let itemContainer = element.querySelector(".items");
-	for(let item of order.items) {
-		let itemElement = createItemElement(item);
-		itemContainer.appendChild(itemElement);
-	}
-
-	return element;
-}
-function updateOrderElementMetaData(element, order) {
-	element.dataset.orderId = order.id;
-	element.dataset.orderNumber = order.orderNumber;
-	element.dataset.status = order.status;
-
-	if(order.status === Api.order.STATUS_PREPARED) {
-		element.classList.add("prepared");
-		element.classList.remove("canceled");
-	}
-	let cancelButton = element.querySelector(".cancel-button");
-	if(order.status === Api.order.STATUS_CANCELED) {
-		element.classList.remove("prepared");
-		element.classList.add("canceled");
-		cancelButton.title = "ausblenden";
-	}
-	else {
-		cancelButton.title = "stornieren";
-	}
-
-	element.querySelector(".order-number").textContent = order.orderNumber;
-	if(order.orderTimestamp) {
-		let tempDate = new Date(order.orderTimestamp);
-		element.dataset.orderTimestamp = tempDate.getTime();
-		element.querySelector(".date").textContent = formatDate(tempDate);
-		element.querySelector(".time").textContent = formatTime(tempDate);
-	}
-	element.querySelector(".price").textContent = order.price.toFixed(2);
 }
 
 function insertElement(oOrderElement) {
@@ -236,7 +144,7 @@ function loadOrders(iVenueID) {
 	]
 	Api.order.getAll(iVenueID, status).then(oResponse => {
 		for(let oOrder of oResponse.json) {
-			let oOrderElement = createElement(oOrder);
+			let oOrderElement = createOrderElement(oOrder);
 			insertElement(oOrderElement);
 		}
 		openEventSource(iVenueID);
---
+++ tickets/script/modules/orderElement.js
@@ -0,0 +1,113 @@
+import Api from "../Api.js";
+import { formatDate, formatTime } from "../functions.js";
+
+function createOrderElement(order=null) {
+	let element = document.createElement("div");
+	element.classList.add("order");
+
+	let orderNumber = document.createElement("div");
+	orderNumber.classList.add("order-number");
+	element.appendChild(orderNumber);
+
+	let date = document.createElement("span");
+	date.classList.add("date");
+	let time = document.createElement("span");
+	time.classList.add("time");
+	element.appendChild(date);
+	element.appendChild(time);
+
+	let itemContainer = document.createElement("div");
+	itemContainer.classList.add("items");
+	element.appendChild(itemContainer);
+
+	let price = document.createElement("span");
+	price.classList.add("price");
+	element.appendChild(price);
+
+	let buttons = {
+		prepared: "abholbereit",
+		pickedup: "abgeholt",
+	};
+	for(let className in buttons) {
+		let button = document.createElement("input");
+		button.classList.add(className);
+		button.type = "button";
+		button.value = buttons[className];
+		element.appendChild(button);
+	}
+	let cancelButton = document.createElement("div");
+	cancelButton.classList.add("cancel-button");
+	cancelButton.title = "stornieren";
+	element.appendChild(cancelButton);
+
+
+	if(order) {
+		updateOrderData(element, order);
+		for(let item of order.items) {
+			let itemElement = createItemElement(item);
+			itemContainer.appendChild(itemElement);
+		}
+	}
+	return element;
+}
+function updateOrderData(element, order) {
+	element.dataset.orderId = order.id;
+	element.dataset.orderNumber = order.orderNumber;
+	element.dataset.status = order.status;
+
+	if(order.status === Api.order.STATUS_PREPARED) {
+		element.classList.add("prepared");
+		element.classList.remove("canceled");
+	}
+	let cancelButton = element.querySelector(".cancel-button");
+	if(order.status === Api.order.STATUS_CANCELED) {
+		element.classList.remove("prepared");
+		element.classList.add("canceled");
+		cancelButton.title = "ausblenden";
+	}
+	else {
+		cancelButton.title = "stornieren";
+	}
+
+	element.querySelector(".order-number").textContent = order.orderNumber;
+	if(order.orderTimestamp) {
+		let tempDate = new Date(order.orderTimestamp);
+		element.dataset.orderTimestamp = tempDate.getTime();
+		element.querySelector(".date").textContent = formatDate(tempDate);
+		element.querySelector(".time").textContent = formatTime(tempDate);
+	}
+	element.querySelector(".price").textContent = order.price.toFixed(2);
+}
+
+
+function createItemElement(item=null) {
+	let element = document.createElement("div");
+	element.classList.add("item");
+
+	let count = document.createElement("span");
+	count.classList.add("count");
+	element.appendChild(count);
+
+	let name = document.createElement("span");
+	name.classList.add("name");
+	element.appendChild(name);
+
+	if(item) {
+		updateItemData(element, item);
+	}
+	return element;
+}
+function updateItemData(element, item) {
+	element.dataset.id = item.id;
+	element.dataset.categoryId = item.itemCategory;
+	element.querySelector(".count").textContent = item.count;
+	element.querySelector(".name").textContent = item.name;
+}
+
+export default {
+	createOrderElement,
+	updateOrderData,
+
+	createItemElement,
+	updateItemData,
+};
