d9ca1797407b1f0658c93d4d6d03416c971b9be8
ItsFlo <flo@bosstrowski.de>
2022-04-20 13:01:11 +0200

    Add config options to use server without ssl
    The https server can now be disabled as well.
    
    Added an option to the http server to process requests normally instead
    of redirecting them to https.

--- server.js
+++ server.js
@@ -10,16 +10,19 @@ import Config from "./modules/Config.js";
 import DispatchManager from "./modules/DispatchManager.js";
 import TicketConfig from "./tickets/TicketConfig.js";
 
-const CONFIG = new Config(JSON.parse(readFileSync(__rootpath+"/config.json")));
-
-TicketConfig.init(CONFIG);
-DispatchManager.init();
-
-
-const httpsOptions = {
-	key: readFileSync(__rootpath+"/ssl/key.pem"),
-	cert: readFileSync(__rootpath+"/ssl/cert.pem"),
-};
+function requestListener(request, response) {
+	let path = getPath(request);
+	response.setHeader("Content-Type", "text/plain");
+	DispatchManager.dispatchManager.request(path, request, response);
+}
+function upgradeListener(request, socket, head) {
+	let path = getPath(request);
+	DispatchManager.dispatchManager.upgrade(path, request, socket, head);
+}
+function connectListener(request, socket, head) {
+	let path = getPath(request);
+	DispatchManager.dispatchManager.connect(path, request, socket, head);
+}
 
 
 function getPath(request) {
@@ -27,47 +30,69 @@ function getPath(request) {
 	return url.pathname;
 }
 
-const ticketServer = https.createServer(httpsOptions);
-let httpsPort = CONFIG.getElement("server.https.port", 443);
-ticketServer.listen(httpsPort);
-
-ticketServer.on("request", (request, response) => {
-	let path = getPath(request);
-	response.setHeader("Content-Type", "text/plain");
-	DispatchManager.dispatchManager.request(path, request, response);
-});
-ticketServer.on("upgrade", (request, socket, head) => {
-	let path = getPath(request);
-	DispatchManager.dispatchManager.upgrade(path, request, socket, head);
-});
-ticketServer.on("connect", (request, socket, head) => {
-	let path = getPath(request);
-	DispatchManager.dispatchManager.connect(path, request, socket, head);
-});
 
 
-if(CONFIG.getElement("server.http.enable", false)) {
-	const httpServer = http.createServer((request, response) => {
-		let host = request.headers.host;
-		let hostParts = host.split(":");
-		if(hostParts.length === 2) {
-			host = hostParts[0];
-		}
-		if(httpsPort !== 443) {
-			host += ":" + httpsPort;
-		}
+function init() {
+	TicketConfig.init(CONFIG);
+	DispatchManager.init();
+}
 
-		if(request.headers.upgrade === "websocket") {
-			response.writeHead(301, {
-				"Location": "wss://"+host+request.url,
+function startServers() {
+	const httpsPort = CONFIG.getElement("server.https.port", 443);
+	if(CONFIG.getElement("server.https.enable", true)) {
+		const httpsOptions = {
+			key: readFileSync(__rootpath+"/ssl/key.pem"),
+			cert: readFileSync(__rootpath+"/ssl/cert.pem"),
+		};
+	
+		const ticketServer = https.createServer(httpsOptions);
+		ticketServer.on("request", requestListener);
+		ticketServer.on("upgrade", upgradeListener);
+		ticketServer.on("connect", connectListener);
+	
+		ticketServer.listen(httpsPort);
+	}
+	
+	
+	if(CONFIG.getElement("server.http.enable", true)) {
+		const httpServer = http.createServer();
+		if(CONFIG.getElement("server.http.redirectToHttps", true)) {
+			httpServer.on("request", (request, response) => {
+				let host = request.headers.host;
+				let hostParts = host.split(":");
+				if(hostParts.length === 2) {
+					host = hostParts[0];
+				}
+				if(httpsPort !== 443) {
+					host += ":" + httpsPort;
+				}
+	
+				if(request.headers.upgrade === "websocket") {
+					response.writeHead(301, {
+						"Location": "wss://"+host+request.url,
+					});
+				}
+				else {
+					response.writeHead(301, {
+						"Location": "https://"+host+request.url,
+					});
+				}
+				response.end();
 			});
 		}
 		else {
-			response.writeHead(301, {
-				"Location": "https://"+host+request.url,
-			});
+			httpServer.on("request", requestListener);
+			httpServer.on("upgrade", upgradeListener);
+			httpServer.on("connect", connectListener);
 		}
-		response.end();
-	});
-	httpServer.listen(CONFIG.getElement("server.http.port", 80));
+	
+		httpServer.listen(CONFIG.getElement("server.http.port", 80));
+	}
+}
+
+
+const CONFIG = new Config(JSON.parse(readFileSync(__rootpath+"/config.json")));
+if(CONFIG.getElement("server.https.enable", true) || CONFIG.getElement("server.http.enable", true)) {
+	init();
+	startServers();
 }
