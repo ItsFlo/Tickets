5e67e1bf66f100a14a1c11063b59702321936458
ItsFlo <flo@bosstrowski.de>
2022-04-20 15:35:12 +0200

    Add buttons to switch dark/light themes

--- tickets/script/DarkMode.js
+++ tickets/script/DarkMode.js
@@ -1,45 +1,119 @@
+import { addLoadListener } from "./functions.js";
+
+const LIGHT = "light";
+const DARK = "dark";
+const THEMES = [
+	LIGHT,
+	DARK,
+];
+
 const DARK_MODE_CLASS = "dark-mode";
-const STORAGE_KEY = "dark-mode";
+const EXPLICIT_THEME_CLASS = "explicit-theme";
+const STORAGE_KEY = "theme-preference";
 
 const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
-function isDarkMode() {
-	let darkModePreference = window.localStorage.getItem(STORAGE_KEY);
-	if(darkModePreference === "true") {
-		return true
-	}
-	else if(darkModePreference === "false") {
-		return false;
+function getTheme() {
+	let themePreference = window.localStorage.getItem(STORAGE_KEY);
+	if(THEMES.includes(themePreference)) {
+		return themePreference;
 	}
 
-	return mediaQuery.matches;
+	return mediaQuery.matches? DARK : LIGHT;
 }
-function updateDarkMode() {
-	let darkMode = isDarkMode();
-	document.documentElement.classList.toggle(DARK_MODE_CLASS, darkMode);
+function updateDocumentCssClasses() {
+	let themePreference = window.localStorage.getItem(STORAGE_KEY);
+	let themeIsExplicit = THEMES.includes(themePreference);
+	document.documentElement.classList.toggle(EXPLICIT_THEME_CLASS, themeIsExplicit);
+
+	let theme = getTheme();
+	document.documentElement.classList.toggle(DARK_MODE_CLASS, theme === DARK);
 }
 
 
-function setDarkModePreference(darkMode=true) {
-	window.localStorage.setItem(STORAGE_KEY, !!darkMode);
-	updateDarkMode();
+function setThemePreference(theme) {
+	if(!THEMES.includes(theme)) {
+		return;
+	}
+	window.localStorage.setItem(STORAGE_KEY, theme);
+	updateDocumentCssClasses();
 }
-function clearDarkModePreference() {
+function toggleThemePreference(theme) {
+	if(!THEMES.includes(theme)) {
+		return;
+	}
+
+	let currentTheme = window.localStorage.getItem(STORAGE_KEY);
+	if(theme !== currentTheme) {
+		setThemePreference(theme);
+	}
+	else {
+		clearThemePreference();
+	}
+}
+function clearThemePreference() {
 	window.localStorage.removeItem(STORAGE_KEY);
-	updateDarkMode();
+	updateDocumentCssClasses();
 }
 
 
+
+function lightButtonListener() {
+	toggleThemePreference(LIGHT);
+}
+function darkButtonListener() {
+	toggleThemePreference(DARK);
+}
+function initButton(button) {
+	let lightButton = document.createElement("span");
+	lightButton.classList.add("light");
+	lightButton.addEventListener("click", lightButtonListener);
+
+	let darkButton = document.createElement("span");
+	darkButton.classList.add("dark");
+	darkButton.addEventListener("click", darkButtonListener);
+
+	button.appendChild(lightButton);
+	button.appendChild(darkButton);
+}
+function initAllButtons() {
+	let buttons = document.querySelectorAll("#darkModeButton");
+	for(let button of buttons) {
+		initButton(button);
+	}
+}
+
+function addStyleSheet() {
+	let element = document.createElement("link");
+	element.type = "text/css";
+	element.rel = "stylesheet";
+	element.href = "/style/darkMode.css";
+	document.head.appendChild(element);
+}
+
+function storageListener(ev) {
+	if(ev.key === STORAGE_KEY) {
+		updateDocumentCssClasses();
+	}
+}
 
 function init() {
-	updateDarkMode();
+	addStyleSheet();
+	updateDocumentCssClasses();
 
-	mediaQuery.addEventListener("change", updateDarkMode);
-	window.addEventListener("storage", updateDarkMode);
+	mediaQuery.addEventListener("change", updateDocumentCssClasses);
+	window.addEventListener("storage", storageListener);
+
+	addLoadListener(initAllButtons);
 }
 init();
 
 export default {
-	setDarkModePreference,
-	clearDarkModePreference,
+	LIGHT,
+	DARK,
+	THEMES,
+
+	setThemePreference,
+	toggleThemePreference,
+	clearThemePreference,
 };
